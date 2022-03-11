0387d75d15412d4b1e5b74e8ea9c59fbd6a1a414
nezuu2 <nezuu_pc2@yahoo.co.jp>
2022-03-12 03:03:50 +0900

    Ttsbuttonを試験的に実装

--- app/src/main/java/com/projectyui/yui/FirstFragment.kt
+++ app/src/main/java/com/projectyui/yui/FirstFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.projectyui.yui.databinding.FragmentFirstBinding
 
@@ -34,6 +35,9 @@ class FirstFragment : Fragment() {
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        binding.buttonTts.setOnClickListener {
+            Toast.makeText(context, "トーストメッセージ", Toast.LENGTH_LONG).show()
         }
     }
 
